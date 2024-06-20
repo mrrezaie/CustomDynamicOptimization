3b6109327ce69b80032236fe4bd73bbdcb7e7b3b
Mohammadreza Rezaie <mrrezaie@outlook.com>
2024-06-20 15:55:14 +0330

    a few improvements

--- casadiFinal.py
+++ casadiFinal.py
@@ -3,89 +3,110 @@ import casadi
 
 ########## functions
 
-def calcMuscleTendonForceMultipliers(AID, activity, NML, NMV, NTL, MTL, PAcos):
+def firstOrderSmoothedActivationDynamics(e,a, t_a=0.015, t_d=0.05, b=0.1):
+
     '''
-    muscle force-length-velocity and tendon force-length curves
-    De Groote et al. (2016)
+    smoothed approximation of first order activation dynamics (Winters1995, Zajac1989)
+    e       muscle excitation,
+    a       muscle activation,
+    t_a     activation time constant 
+                (range: 10 ms for fast to 20 ms for slow muscle)
+                (0.015 s in Thelen2003 and DeGroote2016),
+    t_d     deactivation time constant
+                (range: 30 ms for fast to 50 ms for slow muscle)
+                ()
+                (0.05 s in Thelen2003, 0.06 s in DeGroote2016),
+    b       parameter determining transition smoothness (0.1).
+
+    https://opensimconfluence.atlassian.net/wiki/x/HhkqAw
+    https://github.com/opensim-org/opensim-core/blob/main/OpenSim/Actuators/DeGrooteFregly2016Muscle.cpp#L218-L220
+    https://github.com/opensim-org/opensim-core/blob/main/OpenSim/Simulation/Model/Bhargava2004SmoothedMuscleMetabolics.h#L90-L98
     '''
+    # DeGroote2016
+    f = 0.5 * casadi.tanh(b*(e-a))
+    z = 0.5 + 1.5*a
+    const = (0.5+f)/(t_a*z) + (0.5-f)*z/t_d
+    return const * (e-a) # == da/dt
+
+    # f = 0.5 * casadi.tanh(b*(e-a))
+    # z = 0.5 + 1.5*a
+    # const = t_a*z*(0.5+f) + t_d/z*(0.5-f)
+    # return (e-a) / const 
+
+
+
+def calcDeGrooteTendonCurveShift(kt):
+    # Tendon force-length multipliers
+    c1=0.200; c2=0.995; c3=0.250; shift=0 #1.17507567e-02
+    TFLM35 = c1*np.exp(35*(1.-c2))-c3+shift
+    TFLMkt = c1*np.exp(kt*(1.-c2))-c3+shift
+    shift = TFLM35-TFLMkt
+    return shift
+
+def calcDeGrooteTendonForceLengthMultiplier(NTL, kt, shift):
     # Tendon force-length multiplier (S1)
     c1=0.200; c2=0.995; c3=0.250
-    kt = np.tile(AID['tendonStiffness'], [NTL.shape[0],1])
-    shift = np.tile(AID['tendonCurveShift'], [NTL.shape[0],1])
-    NTF = c1*np.exp(kt*(NTL-c2))-c3+shift
+    NTF = c1*casadi.exp(kt*(NTL-c2))-c3+shift
+    # ignore negative tendon forces
+    NTF = casadi.if_else(NTF<=0.0, 0.0, NTF)
+    return NTF
 
+def calcDeGrooteActiveFiberForceLengthMultiplier(NML):
     # Active muscle force-length multiplier (S2)
+
+    # default De Groote 
     b11=+0.815; b21=+1.055; b31=+0.162; b41=+0.063
     b12=+0.433; b22=+0.717; b32=-0.030; b42=+0.200
-    b13=+0.100; b23=+1.000; b33=+0.354; b43=+0.000 
-    NFF1 = b11*np.exp(-0.5*(NML-b21)**2 / (b31+b41*NML)**2)
-    NFF2 = b12*np.exp(-0.5*(NML-b22)**2 / (b32+b42*NML)**2)
-    NFF3 = b13*np.exp(-0.5*(NML-b23)**2 / (b33+b43*NML)**2)
+    b13=+0.100; b23=+1.000; b33=+0.354; b43=+0.000
+    NFF1 = b11*casadi.exp(-0.5*(NML-b21)**2 / (b31+b41*NML)**2)
+    NFF2 = b12*casadi.exp(-0.5*(NML-b22)**2 / (b32+b42*NML)**2)
+    NFF3 = b13*casadi.exp(-0.5*(NML-b23)**2 / (b33+b43*NML)**2)
     AMFLM = NFF1+NFF2+NFF3
+    # ignore normalized active forces more than 1
+    AMFLM = casadi.if_else(AMFLM>=1.0, 1.0, AMFLM)
+    return AMFLM
 
-    # Pasive muscle force multiplier (S3)
-    kp=4.0; e0=0.6
-    PMFLM = (np.exp(kp*(NML-1)/e0)-1) / (np.exp(kp)-1)
-
+def calcDeGrooteFiberForceVelocityMultiplier(NMV):
     # Active muscle force-velocity multiplier (S4)
     d1=-0.318; d2=-8.149; d3=-0.374; d4=+0.886
-    AMFVM = d1*np.log(d2*NMV+d3 + np.sqrt((d2*NMV+d3)**2+1))+d4   
+    AMFVM = d1*casadi.log(d2*NMV+d3 + casadi.sqrt((d2*NMV+d3)**2+1))+d4
+    return AMFVM
 
-    # Total muscle force-length-velocity force multiplier
-    NMFAT = (activity*AMFLM*AMFVM + PMFLM) * PAcos
+def calcDeGrootePassiveFiberForceLengthMultiplier(NML):
+    # Pasive muscle force multiplier (S3)
+    kp=4.0; e0=0.6
+    PMFLM = (casadi.exp(kp*(NML-1)/e0) - 1) / (casadi.exp(kp) - 1)
+    # ignore negative passive forces
+    PMFLM = casadi.if_else(PMFLM<=0.0, 0.0, PMFLM)
+    return PMFLM
 
-    return NTF, NMFAT
-
-
-
-def ActivationDynamics(e,a, tact=0.015, tdeact=0.06, b=0.1):
-    '''
-    by Winters 1995
-    De Groote et al. 2016 Eq(1) and (2)
-    e       muscle excitation, 
-    a       muscle activation,
-    tact    activation time constant (0.015 s),
-    tdeact  deactivation time constant (0.060 s),
-    b       parameter determining transition smoothness (0.1).
-    '''
-    
-    f = 0.5 * np.tanh(b*(e-a))
-    constant = ((f+0.5)/(tact*(0.5+1.5*a)) + (0.5+1.5*a)/tdeact*(-f+0.5))
-    return constant * (e-a) # == da/dt
-
-
-
-def calcShift(kt):
-    # force-length multipliers
-    c1=0.200; c2=0.995; c3=0.250; shift=0
-    TFLM35 = c1*np.exp(35*(1.-c2))-c3+0.
-    TFLMkt = c1*np.exp(kt*(1.-c2))-c3+0.
-    return TFLM35-TFLMkt
 
 
 
 # data
-AID = dict( np.load('./dataDynamic.npz') )
+AID = dict( np.load('./data.npz') )
 
-# adjust tendon stiffness (DeGroote muscle)
-stiffness, shift = list(),list()
+# adjust tendon stiffness and curve shift (DeGroote muscle)
+stiffness,shift = list(),list()
 for mName in AID['nameMuscles']:
     if mName.split('_')[0] in ['soleus', 'gasmed', 'gaslat']: # triceps surae
-        stiffness.append(15)
-        shift.append( calcShift(15) )
+        value = 20
+        stiffness.append(value)
+        shift.append( calcDeGrooteTendonCurveShift(value))
     elif mName.split('_')[0] in ['recfem', 'vasint', 'vaslat', 'vasmed']: # quadriceps
-        stiffness.append(20)
-        shift.append( calcShift(20) )
+        value = 25
+        stiffness.append(value)
+        shift.append( calcDeGrooteTendonCurveShift(value))
     else:
         stiffness.append(35)
-        shift.append( calcShift(35) )
+        shift.append( 0)
 
 AID['tendonStiffness']  = np.array(stiffness)
 AID['tendonCurveShift'] = np.array(shift)
 
 # parameters
-iN, iM = AID['iMTL'].shape
-iDt = 0.01 # time interval
+iN, iM = AID['MTL'].shape
+iDt = 0.01 # time interval (mesh frequency == 100 Hz)
 
 opti = casadi.Opti()
 
@@ -120,18 +141,18 @@ opti.subject_to( opti.bounded(0.2,_NML,2) )
 opti.subject_to( opti.bounded(-1,_NMV,1) )
 opti.subject_to( 1.0 <= casadi.vec(_NTL))
 
+_TL = _NTL*np.tile(AID['TLslk'],[iN,1])
 _ML = _NML * np.tile(AID['MLopt'],[iN,1])
-_MLAT = AID['iMTL'] - _NTL*np.tile(AID['TLslk'],[iN,1])
+_MLAT = AID['MTL'] - _TL
 _PAcos = _MLAT / _ML
 
 # bounds on on fiber pennation angle, min == 84.26deg or arccos(0.1)
-opti.subject_to( 0.1 <= casadi.vec(_PAcos) )
-
+opti.subject_to( opti.bounded(0.1,_PAcos,1))
 
 ########## constraints
 
 # equality constraint on fiber geometry
-opti.subject_to( _ML**2 == _MLAT**2 + np.tile(AID['height'],[iN,1])**2 )
+opti.subject_to( _ML == casadi.sqrt( _MLAT**2 + np.tile(AID['muscleHeight'],[iN,1])**2) )
 
 # backward Euler method
 _NML_dot = (_NML[1:,:]-_NML[:-1,:]) / iDt
@@ -140,42 +161,65 @@ _act_dot = (_act[1:,:]-_act[:-1,:]) / iDt
 _NML_dot = casadi.vertcat( np.zeros([1,iM]), _NML_dot)
 _act_dot = casadi.vertcat( np.zeros([1,iM]), _act_dot)
 
-# equality constraint on fiber length derivative and velocity
+# equality constraint on fiber length derivative and fiber velocity
 opti.subject_to( _NML_dot / np.tile(AID['CVmax'],[iN,1]) == _NMV )
 
 # equality constraint on muscle activation derivative and activation dynamics
-opti.subject_to( _act_dot == ActivationDynamics(_exc, _act, tact=0.015, tdeact=0.06, b=0.1) )
+opti.subject_to( _act_dot == firstOrderSmoothedActivationDynamics(_exc, _act) )
+
+# normalized tendon force
+_NTF = calcDeGrooteTendonForceLengthMultiplier(_NTL, 
+    np.tile(AID['tendonStiffness'],  [iN,1]),
+    np.tile(AID['tendonCurveShift'], [iN,1]) )
+
+# normalized fiber force
+_AMFLM = calcDeGrooteActiveFiberForceLengthMultiplier(_NML)
+_PMFLM = calcDeGrootePassiveFiberForceLengthMultiplier(_NML)
+_AMFVM = calcDeGrooteFiberForceVelocityMultiplier(_NMV)
+_NMFAT = (_act*_AMFLM*_AMFVM + _PMFLM) * _PAcos
 
 # equality constraint on fiber and tendon force equilibrium
-NTF, NMFAT = calcMuscleTendonForceMultipliers(AID, _act, _NML, _NMV, _NTL, AID['iMTL'], _PAcos)
-opti.subject_to( NTF == NMFAT )
+opti.subject_to( _NTF == _NMFAT )
 
 # equality constraint on moment equilibrium
 
-for ci,cName in enumerate(AID['include']):
-    _tau       = AID['tau'][:,ci]
+# 'hip_flexion_r', 'hip_adduction_r', 'hip_rotation_r',                                                                                                    
+# 'hip_flexion_l', 'hip_adduction_l', 'hip_rotation_l',                                                                                                    
+# 'knee_angle_r', 'knee_angle_l', 'ankle_angle_r', 'ankle_angle_l'
+
+for ci,cName in enumerate(AID['include']): # loop over the coordinates
+    _tau       = AID['tau'][:,ci] # joint moment from ID
     _momentArm = AID[cName]
-    _moment    = casadi.sum2( _momentArm * NMFAT*np.tile(AID['IFmax'],[iN,1]) )
+    _moment    = casadi.sum2( _momentArm * _NTF*np.tile(AID['IFmax'],[iN,1]) ) # muscle moment
     opti.subject_to( _moment == _tau )
 
-muscleExcitation = casadi.sumsqr(_exc) # 1.0297743e+02 (32) 9.18s
+muscleExcitation = casadi.sumsqr(_exc) # 9.7998063e+01 (23) 8.835s
 
 opti.minimize( muscleExcitation )
 
 # plugin_options
-p_opts = {'expand':True, 
-          'detect_simple_bounds':True}
+p_opts = {
+        'expand': True, 
+        'detect_simple_bounds': True,
+        # "print_time": False,
+        }
 
 # solver_options
-s_opts = {'max_iter':1e+4, 
-          'linear_solver':'mumps', 
-          'tol':1e-6, 
-          # 'hessian_approximation': 'limited-memory',
-          'nlp_scaling_method':'gradient-based' }
+s_opts = {
+        'linear_solver': 'mumps',
+        # 'print_level': 5,
+        # 'print_timing_statistics': 'yes',
+        'output_file': './IPOPT_output.txt',
+        'max_iter':        1e+3,
+        'tol':             1e-5,
+        'constr_viol_tol': 1e-4,
+        'compl_inf_tol':   1e-4,
+        'print_user_options': 'yes',
+        # 'hessian_approximation': 'limited-memory',
+        # 'mu_strategy': 'adaptive',
+        'nlp_scaling_method':'gradient-based',#'none'
+        }
           
 opti.solver('ipopt', p_opts, s_opts)
 
 solver = opti.solve()
-
-
-
